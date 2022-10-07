fe14761d21e8e246e6af7c5a50cab0847074f239
JanvandenBrand <janvandenbrand@gmail.com>
2022-10-07 08:43:38 +0200

    tested and fixed caesarciphershift

--- src/caesarshift.py
+++ src/caesarshift.py
@@ -2,6 +2,10 @@ from string import ascii_lowercase
 
 
 def caesarshiftcipher(message, shift):
+    """
+    This implementation close follows the (faulty) java 
+    example from the MOOC and should fail.
+    """
     sb = []
     current_char = []
     length = len(message)
@@ -13,7 +17,6 @@ def caesarshiftcipher(message, shift):
         current_char = [
             j for j in range(len(list(ascii_lowercase)))
             if list(ascii_lowercase)[j] == message[i]][0]
-        # sb.append(current_char)
 
         if (letters[current_char] > 'z' or letters[current_char] < 'a'):
             return 'invalid'
@@ -25,3 +28,39 @@ def caesarshiftcipher(message, shift):
         sb.append(letters[current_char + shift])
 
     return "".join(sb)
+
+
+# FOr debugging
+# caesarshiftcipher("nop", -3)
+
+
+def caesarshiftcipher_fixed(message, shift):
+    """
+    This should work. 
+    """
+    sb = []
+    current_char = []
+    # make message lower case
+    length = len(message)
+    letters = list(ascii_lowercase)
+
+    shift = shift % 26
+
+    for i in range(0, length):
+        current_char = [
+            j for j in range(len(list(ascii_lowercase)))
+            if list(ascii_lowercase)[j] == message[i]][0]
+
+        if (current_char > 25 or current_char < 0):
+            return 'invalid'
+        elif (current_char + shift > 25):
+            current_char = current_char - 26
+        elif (current_char + shift < 0):
+            current_char = current_char + 26
+
+        sb.append(letters[current_char + shift])
+
+    return "".join(sb)
+
+
+# caesarshiftcipher_fixed("nop", -3)
--- tests/test_caesarshift.py
+++ tests/test_caesarshift.py
@@ -2,5 +2,115 @@ import pytest
 import sys
 import os
 sys.path.append(os.path.abspath("./src/"))
-from caesarshift import caesarshiftcipher
+from caesarshift import (
+    caesarshiftcipher,
+    caesarshiftcipher_fixed
+)
 
+# Test cases
+# letter shifts right between a and z: c shift +3 -> f
+# letter shifts left between z and a: n shift -3 -> k
+# letter shifts right past z: x shift +3 -> a
+# letter shifts left past a: b shift -3 -> y
+# boundaries:
+# a - 1 -> z
+# z + 1 -> a
+# a + 25 -> z
+# z - 25 -> a 
+# zero shift
+
+
+@pytest.mark.parametrize("message, shift, result",
+    [
+        pytest.param(
+            'abc', 3, 'def',
+            id="letter shifts right between a and z"
+        ),
+        pytest.param(
+            'nop', -3, 'klm',
+            id="letter shifts left between z and a"
+        ),
+        pytest.param(
+            'xyz', 3, 'abc',
+            id="letter shifts right past z"
+        ),
+        pytest.param(
+            'abc', -3, 'xyz',
+            id="letter shifts left past a"
+        ),
+        pytest.param(
+            'abc', -1, 'zab',
+            id="a out boundry"
+        ),
+        pytest.param(
+            'xyz', +1, 'yza',
+            id="z out boundry"
+        ),
+        pytest.param(
+            'a', 25, 'z',
+            id='a in boundry'
+        ),
+        pytest.param(
+            'z', -25, 'a',
+            id='z in boundry'
+        ),
+        pytest.param(
+            'abc', 0, 'abc',
+            id="zero shift"
+        )
+    ]
+)
+def test_caesarshiftcipher(message, shift, result):
+    expected = result
+    actual = caesarshiftcipher(message, shift)
+    message = "Expected {0}, but caesarshiftcipher({1}, {2},\
+        returned {3}".format(expected, message, shift, actual)
+    assert expected == actual
+
+
+@pytest.mark.parametrize("message, shift, result",
+    [
+        pytest.param(
+            'abc', 3, 'def',
+            id="letter shifts right between a and z"
+        ),
+        pytest.param(
+            'nop', -3, 'klm',
+            id="letter shifts left between z and a"
+        ),
+        pytest.param(
+            'xyz', 3, 'abc',
+            id="letter shifts right past z"
+        ),
+        pytest.param(
+            'abc', -3, 'xyz',
+            id="letter shifts left past a"
+        ),
+        pytest.param(
+            'abc', -1, 'zab',
+            id="a out boundry"
+        ),
+        pytest.param(
+            'xyz', +1, 'yza',
+            id="z out boundry"
+        ),
+        pytest.param(
+            'a', 25, 'z',
+            id='a in boundry'
+        ),
+        pytest.param(
+            'z', -25, 'a',
+            id='z in boundry'
+        ),
+        pytest.param(
+            'abc', 0, 'abc',
+            id="zero shift"
+        )
+    ]
+)
+def test_caesarshiftcipher_fixed(message, shift, result):
+    expected = result
+    actual = caesarshiftcipher_fixed(message, shift)
+    message = "Expected {0}, but caesarshiftcipher({1}, {2},\
+        returned {3}".format(expected, message, shift, actual)
+    assert expected == actual
